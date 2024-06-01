c979a14e3ce9aafde32cb1cb34f45f9ee01adb8f
Nicholas Adamou <nicholasadamouemail@gmail.com>
2024-06-01 07:51:19 -0400

    init

---
+++ .github/README.md
@@ -0,0 +1,13 @@
+# 'xcode' module
+
+This repository is designed to be used as a submodule to the [`set-me-up` blueprint](https://github.com/dotbrains/set-me-up-blueprint) repository.
+
+⚠️ **Note**: This repository should not be used as a standalone script because it has a dependency on an existing 'set-me-up' configuration already installed on your machine.
+
+## _Why abstract these modules to an external repository?_
+
+Please see the [universal modules documentation](https://github.com/dotbrains/set-me-up-universal-modules#why-abstract-these-modules-to-an-external-repository) for more details on this point.
+
+## License
+
+The code is available under the [MIT license](LICENSE).
---
+++ xcode.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+# shellcheck source=/dev/null
+
+declare current_dir &&
+    current_dir="$(dirname "${BASH_SOURCE[0]}")" &&
+    cd "${current_dir}" &&
+    source "$HOME/set-me-up/dotfiles/utilities/utilities.sh"
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+agree_with_xcode_license() {
+
+    # Automatically agree to the terms of the `Xcode` license.
+    # https://github.com/alrra/dotfiles/issues/10
+
+    sudo xcodebuild -license accept &>/dev/null
+
+}
+
+are_xcode_command_line_tools_installed() {
+
+    xcode-select --print-path &>/dev/null
+
+}
+
+install_xcode() {
+
+    # If necessary, prompt user to install `Xcode`.
+
+    if ! is_xcode_installed; then
+        open "macappstores://itunes.apple.com/en/app/xcode/id497799835"
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until `Xcode` is installed.
+
+    until is_xcode_installed; do
+        sleep 5
+    done
+
+}
+
+install_xcode_command_line_tools() {
+
+    # If necessary, prompt user to install
+    # the `Xcode Command Line Tools`.
+
+    xcode-select --install &>/dev/null
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Wait until the `Xcode Command Line Tools` are installed.
+
+    until are_xcode_command_line_tools_installed; do
+        sleep 5
+    done
+
+}
+
+is_xcode_installed() {
+
+    [ -d "/Applications/Xcode.app" ]
+
+}
+
+set_xcode_developer_directory() {
+
+    # Point the `xcode-select` developer directory to
+    # the appropriate directory from within `Xcode.app`.
+    #
+    # https://github.com/alrra/dotfiles/issues/13
+
+    sudo xcode-select -switch "/Applications/Xcode.app/Contents/Developer" &>/dev/null
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    # Check if OS is macOS
+    if ! is_macos; then
+        error "This script is only for macOS!"
+        return 1
+    fi
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    install_xcode_command_line_tools
+    install_xcode
+    set_xcode_developer_directory
+    agree_with_xcode_license
+
+}
+
+main
